7424d47a649a9da332d9104dfcc434febfd371a1
gustavohees <gustavocesarhees@gmail.com>
2025-05-30 20:45:14 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuibZ7InKvmTOYkk2Ej2PZvMMEfh-5ycI",
+  authDomain: "guilhermejuliana-edb62.firebaseapp.com",
+  projectId: "guilhermejuliana-edb62",
+  storageBucket: "guilhermejuliana-edb62.appspot.com", // ← corrigido aqui!
+  messagingSenderId: "133407955632",
+  appId: "1:133407955632:web:9c21e3b1fa9bde33dc3975"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
